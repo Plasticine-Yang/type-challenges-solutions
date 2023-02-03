402a6fcc8a00a51e1d196221cf425892baea8908
plasticine <975036719@qq.com>
2023-02-03 11:07:27 +0800

    feat: medium - 296 - Permutation

---
+++ src/medium/296-Permutation.ts
@@ -0,0 +1,55 @@
+/*
+  296 - Permutation
+  -------
+  by Naoto Ikuno (@pandanoir) #medium #union
+
+  ### Question
+
+  Implement permutation type that transforms union types into the array that includes permutations of unions.
+
+  ```typescript
+  type perm = Permutation<'A' | 'B' | 'C'>; // ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']
+  ```
+
+  > View on GitHub: https://tsch.js.org/296
+*/
+
+/* _____________ Your Code Here _____________ */
+
+// 借助 Acc 遍历联合类型的每一个元素，逐个添加到结果数组中，剩余的元素使用 Exclude 插入即可
+type Permutation<T, Acc = T> =
+  // base case
+  [T] extends [never]
+    ? []
+    : // iterates item of union type `T`
+    Acc extends T
+    ? [Acc, ...Permutation<Exclude<T, Acc>>]
+    : []
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Permutation<'A'>, ['A']>>,
+  Expect<
+    Equal<
+      Permutation<'A' | 'B' | 'C'>,
+      ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']
+    >
+  >,
+  Expect<
+    Equal<
+      Permutation<'B' | 'A' | 'C'>,
+      ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']
+    >
+  >,
+  Expect<Equal<Permutation<boolean>, [false, true] | [true, false]>>,
+  Expect<Equal<Permutation<never>, []>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/296/answer
+  > View solutions: https://tsch.js.org/296/solutions
+  > More Challenges: https://tsch.js.org
+*/
